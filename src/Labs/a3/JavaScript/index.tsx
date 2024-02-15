70a2f5e5e732712c4930f93595933bf95d74d649
ami-yokoyama1 <ami.yokoyama1@gmail.com>
2024-02-14 16:23:31 -0800

    lab

--- src/Labs/a3/JavaScript/index.tsx
+++ src/Labs/a3/JavaScript/index.tsx
@@ -1,6 +1,19 @@
+import AddingAndRemovingDataToFromArrays from "./arrays/AddingAndRemovingDataToFromArrays";
+import ArrayIndexAndLength from "./arrays/ArrayIndexAndLength";
+import FilterFunction from "./arrays/FilterFunction";
+import FindFunction from "./arrays/FindFunction";
+import FindIndex from "./arrays/FindIndex";
+import ForLoops from "./arrays/ForLoops";
+import MapFunction from "./arrays/MapFunction";
+import WorkingWithArrays from "./arrays/WorkingWithArrays";
 import IfElse from "./conditionals/IfElse";
 import TernaryOperator from "./conditionals/TernaryOperator";
+import ArrowFunctions from "./functions/ArrowFunctions";
 import ES5Functions from "./functions/ES5Functions";
+import FunctionParenthesisAndParameters from "./functions/FunctionParenthesisAndParameters";
+import ImpliedReturn from "./functions/ImpliedReturn";
+import JsonStringify from "./json/JsonStringify";
+import TemplateLiterals from "./string/TemplateLiterals";
 import BooleanVariables from "./variables/BooleanVariables";
 import VariableTypes from "./variables/VariableTypes";
 import VariablesAndConstants from "./variables/VariablesAndConstants";
@@ -10,6 +23,19 @@ function JavaScript(){
     return(
         <div>
             <h3>JavaScript</h3>
+            <TemplateLiterals />
+            <FilterFunction />
+            <FindIndex />
+            <FindFunction />
+            <JsonStringify />
+            <MapFunction />
+            <ForLoops />
+            <AddingAndRemovingDataToFromArrays />
+            <ArrayIndexAndLength />
+            <WorkingWithArrays />
+            <FunctionParenthesisAndParameters />
+            <ImpliedReturn />
+            <ArrowFunctions />
             <ES5Functions />
             <TernaryOperator />
             <IfElse />
