3abd22e4675deb24859c9dc5573d3e6f71012c53
ami-yokoyama1 <ami.yokoyama1@gmail.com>
2024-02-23 23:37:18 -0800

    secondCommit

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
 
 function Assignment4(){
+    function sayHello(){
+        alert("Hello");
+    }
     return(
         <div>
             <h2>Assignment 4</h2>
+            <StringStateVariables />
+            <BooleanStateVariables />
+            <Counter />
+            <EventObject />
+            <PassingFunctions theFunction={sayHello} />
+            <ClickEvent />
+            <PassingDataOnEvent />
         </div>
     );
 }
