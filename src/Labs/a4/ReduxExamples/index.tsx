fd6e983b370e6fc4a5ac82e4d645277a015d0e4a
ami-yokoyama1 <ami.yokoyama1@gmail.com>
2024-03-05 13:15:30 -0800

    lab4

--- src/Labs/a4/ReduxExamples/index.tsx
+++ src/Labs/a4/ReduxExamples/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import CounterReducer from "./CounterRedux/counterReducer";
+import HelloRedux from "./HelloRedux";
+import CounterRedux from "./CounterRedux";
 
 const ReduxExamples = () => {
   return(
     <div>
       <h2>Redux Examples</h2>
-      {/* counterReducer: {1} */}
+      <HelloRedux />
+      <CounterRedux />
       
     </div>
   );
