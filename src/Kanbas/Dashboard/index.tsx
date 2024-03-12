98cbcbcedcb7972cff6fabc0c4788fa759263fe7
ami-yokoyama1 <ami.yokoyama1@gmail.com>
2024-03-12 11:55:22 -0700

    before3.2

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,38 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from "react-router-dom";
 import courses from "../Database/courses.json"
+import * as db from "../Database";
 
 function Dashboard(){
-    return (
-        <>
-            <div className="p-4">
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState({
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "/images/reactjs.jpg"
+  });
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else{
+          return c;
+        }
+      })
+    );
+  };
+  const addNewCourse = () => {
+    const newCourse = { ...course, _id: new Date().getTime().toString() };
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+  return (
+    <div className="p-4">
       <h1>Dashboard</h1> <hr />
+      <h5>Course</h5>
+      <input value={course.name} className="form-control"
+             onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+      <input value={course.number} className="form-control"
+             onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
+      <input value={course.startDate} className="form-control" type="date"
+             onChange={(e) => setCourse({ ...course, startDate: e.target.value }) }/>
+      <input value={course.endDate} className="form-control" type="date"
+             onChange={(e) => setCourse({ ...course, endDate: e.target.value }) } />
+      <button onClick={addNewCourse} >Add</button>
+
+      <button onClick={updateCourse} >
+        Update
+      </button>
+
       <h2>Published Courses (12)</h2> <hr />
       <div className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
-         {courses.map((course) => (<div className="col" style={{width: "300px"}}>
-            <div className="card">
-              <img src="/images/reactjs.png" className="card-img-top"
-                   style={{maxHeight: "150px"}}/>
-              <div className="card-body">
-                <Link className="card-title" 
-                to={`/Kanbas/Courses/${course._id}`}
-                   style={{textDecoration: "none", color: "navy", fontWeight: "bold"}}>
-                   {course.name}</Link>
-                <p className="card-text">Full Stack software developer</p>
-                <Link to={"#"} className="btn btn-primary"> Go </Link>
+        {courses.map((course) => (
+          <div key={course._id} className="col" style={{ width: "300px" }}>
+              <div className="card">
+                <img src="/images/reactjs.png" className="card-img-top"
+                  style={{maxHeight: "150px"}}/>
+                <div className="card-body">
+                  <Link className="card-title" 
+                  to={`/Kanbas/Courses/${course._id}`}
+                    style={{textDecoration: "none", color: "navy", fontWeight: "bold"}}>
+                    {course.name} <br />
+                     <button className="btn btn-primary" onClick={(event) => {
+                      event.preventDefault();
+                      setCourse(course);
+                      }}> Edit
+                      </button>
+
+                      <button className="btn btn-primary" onClick={(event) => {
+                          event.preventDefault();
+                          deleteCourse(course._id);
+                        }}> Delete
+                      </button>
+                    </Link>
+                  <p className="card-text">Full Stack software developer</p>
+                  <Link to={"#"} className="btn btn-primary"> Go </Link>
+                </div>
               </div>
             </div>
-          </div>))}
+          ))}
         </div>
-        </div>
-        <pre>
-            <code>{JSON.stringify(courses, null, 2)}</code>
-        </pre>
       </div>
-        </>
-        
-    );
+        {/* <pre>
+            <code>{JSON.stringify(courses, null, 2)}</code>
+        </pre> */}
+    </div>       
+  );
 }
-
 export default Dashboard;
